9ab379c41deb501845b982c15afcc1389baf5507
mattvryan <matt@mvryan.org>
2011-06-07 01:20:53 -0600

    Added parameters to AWSRestRequest constructor.
    Added the following parameters to the AWSRestRequest constructor:
     - aws_access_key, default is AWS_ACCESS_KEY environment variable
     - aws_secret_key, default is AWS_SECRET_KEY environment variable
     - secure_http, default is True
     - time_offset, default is 0
    A ValueError will be raised if the access key or the secret key is not provided.  Two unit tests were added to verify the error checking in the constructor for whether an access key and a secret key are provided.

--- src/py/lib/Zoomulus/AWSRestRequest.py
+++ src/py/lib/Zoomulus/AWSRestRequest.py
@@ -8,15 +8,22 @@ Copyright (c) 2011 zoomulus.org. All rights reserved.
 """
 
 import sys
+import os
 import hmac
 import hashlib
 import base64
 
 
-
 class AWSRestRequest:
-	def __init__(self,aws_secret_key):
+	def __init__(self,aws_access_key=os.environ.get('AWS_ACCESS_KEY'),aws_secret_key=os.environ.get('AWS_SECRET_KEY'),secure_http=True,time_offset=0):
+		self.aws_access_key = aws_access_key
 		self.aws_secret_key = aws_secret_key
+		self.secure_http = secure_http
+		self.time_offset = time_offset
+		if not aws_access_key or not len(aws_access_key):
+			raise ValueError, 'AWS Access Key required'
+		if not aws_secret_key or not len(aws_secret_key):
+			raise ValueError, 'AWS Secret Key required'
 	def generate_signature(self,s):
 		return base64.b64encode(hmac.new(self.aws_secret_key,s,hashlib.sha1).digest())
 
--- src/py/tests/unittests/Zoomulus/test_AWSRestRequest.py
+++ src/py/tests/unittests/Zoomulus/test_AWSRestRequest.py
@@ -7,6 +7,7 @@ Created by Matt Ryan on 2011-06-04.
 Copyright (c) 2011 zoomulus.org. All rights reserved.
 """
 
+import os
 import unittest
 
 import Zoomulus.AWSRestRequest as awsreq
@@ -15,8 +16,29 @@ import Zoomulus.AWSRestRequest as awsreq
 class test_AWSRestRequest(unittest.TestCase):
 	def setUp(self):
 		pass
+	def run_test_without_key_data(self,keydataname):
+		prev_keydata = None
+		try:
+			prev_keydata = os.environ[keydataname]
+			os.environ[keydataname] = ''
+		except KeyError:
+			pass
+		try:
+			req = awsreq.AWSRestRequest()
+			self.fail()
+		except ValueError:
+			pass
+		except Exception:
+			self.fail()
+		finally:
+			if prev_keydata is not None:
+				os.environ[keydataname] = prev_keydata
+	def test_init_with_no_access_key(self):
+		self.run_test_without_key_data('AWS_ACCESS_KEY')
+	def test_init_with_no_secret_key(self):
+		self.run_test_without_key_data('AWS_SECRET_KEY')
 	def test_generate_signature(self):
-		req = awsreq.AWSRestRequest('MySecretKey')
+		req = awsreq.AWSRestRequest('DummyAccessKey','MySecretKey')
 		self.assertEquals(req.generate_signature('FakeRequestDescription'),'wZs+py3LaQiMBOmpy0zSYD5nHC0=')
 
     
